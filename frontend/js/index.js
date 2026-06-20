4bd6febb2fdfef507b11fde4a56de4f4394c3a90
24wh1a0529-sys <24wh1a0529@bvrithyderabad.edu.in>
2026-06-20 10:50:28 +0530

    random user

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,2 +1,12 @@
 //alert("Hello from js");
 console.log("Hello from js");
+
+const gender = document.getElementById("gender");
+
+gender.addEventListener("click", function () {
+    if (gender.innerText === "Male") {
+        gender.innerText = "Female";
+    } else {
+        gender.innerText = "Male";
+    }
+});
